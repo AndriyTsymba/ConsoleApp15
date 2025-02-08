adbbc75aca2bf6d1a561c5a5b6bff17174ca1b80
Kommandor <a.tsymbaliuk2007@gmail.com>
2025-02-08 13:58:50 +0200

    add task4

--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -12,9 +12,9 @@ namespace ConsoleApp15
         {
             int[] numbers = { -7, 14, 21, -3, 49, 7, 5, 14, -7, 28, 35, -35, 0, 42, -42 };
 
-            Func<int[], int> countMultiplesOfSeven = arr => arr.Count(n => n % 7 == 0 && n != 0);
+            Func<int[], int> countPositiveNumbers = arr => arr.Count(n => n > 0);
 
-            Console.WriteLine($"Кількість чисел, кратних 7: {countMultiplesOfSeven(numbers)}");
+            Console.WriteLine($"Кількість позитивних чисел: {countPositiveNumbers(numbers)}");
         }
     }
 }
